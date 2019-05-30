7ecf1c64f7a63853513ded04c0d502cf1ac45ec4
finn <finn@163.com>
2019-05-30 14:29:33 +0800

    把本地工程添加到git

---
+++ project/git-test-project/src/com/bitland/git/FirstClass.java
@@ -0,0 +1,9 @@
+package com.bitland.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git 睥睨寰宇荡红尘，万里天剑尽苍穹。");
+        System.out.println("git 伏羲八卦，乾坤无量。");
+        System.out.println("git 世事如棋，乾坤莫测，笑尽英雄。");
+    }
+}
