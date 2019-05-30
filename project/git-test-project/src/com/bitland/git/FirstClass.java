e46766bedd5311a78eca8733d97a7d4b31628cb5
finn <finn@163.com>
2019-05-30 16:03:45 +0800

    修改了firstClass222

--- project/git-test-project/src/com/bitland/git/FirstClass.java
+++ project/git-test-project/src/com/bitland/git/FirstClass.java
@@ -5,10 +5,13 @@ public class FirstClass {
         System.out.println("git 睥睨寰宇荡红尘，万里天剑尽苍穹。");
         System.out.println("git 伏羲八卦，乾坤无量。");
         System.out.println("git 世事如棋，乾坤莫测，笑尽英雄。");
+        //雪藏夜，夜藏锋
+        System.out.println("脑中玄机用不尽，统辖文武半边天。");
     }
 
     public  void test(){
         int a = 0;
         a += 10;
+        System.out.println(a);
     }
 }
